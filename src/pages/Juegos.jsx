ba0d8a69ba6c455e6fe1926c6cdb13f436ec13dc
unknown <marg.ec256@gmail.com>
2025-07-06 23:29:52 -0300

    Agregue funcionalidad de navegacion a travez de paginas en al seccion de juegos

--- src/pages/Juegos.jsx
+++ src/pages/Juegos.jsx
@@ -28,22 +28,38 @@ export const Juegos = () => {
   const [listaJuegos, SetListaJuegos] = useState([]);
   const [showLoading, setShowLoading] = useState(true);
 
-  const getJuegosApi = async () => {
+  const [pageNum, setPageNum] = useState(1);
+
+  const getJuegosApi = async (page = pageNum) => {
     try {
-      const response = await fetch(`https://api.rawg.io/api/games?key=957f6a2b15fa49f68a9bb400ac60e7f0&page_size=25`);
+      const response = await fetch(`https://api.rawg.io/api/games?key=957f6a2b15fa49f68a9bb400ac60e7f0&page=${page}&page_size=20`);
       const data = await response.json();
       SetListaJuegos(data.results);
-      console.log(data.results);
     } catch (error) {
       console.error("Error fetching games:", error);
+    }
+    finally {
+      setShowLoading(false)
     }
   }
   useEffect(() => {
     getJuegosApi();
-    setTimeout(() => {
-      setShowLoading(false)
-    }, 1000);
-  }, [])
+    setShowLoading(true)
+    window.scrollTo(0,0)
+  }, [pageNum])
+  
+  
+  const nextPage = async() => {
+    setPageNum(pageNum + 1);
+  }
+
+  const backPage = async() => {
+    if(pageNum > 1) {
+      setPageNum(pageNum - 1)
+    }
+  }
+
+  
 
   return (
     <section className='juegos'>
@@ -62,9 +78,9 @@ export const Juegos = () => {
                 <div className='bubble'></div>
                 <div className='bubble b-1'></div>
                 <div className='juegos_list_btns'>
-                  <NavLink to='/categorias'>2</NavLink>
-                  <h4>3</h4>
-                  <NavLink to='/categorias'>4</NavLink>
+                  <button onClick={backPage} className={pageNum === 1 && 'hidden'}>{pageNum > 1 ? pageNum -1 : ''}</button>
+                  <h4>{pageNum}</h4>
+                  <button onClick={nextPage}>{pageNum + 1}</button>
                 </div>
               </div>
             )

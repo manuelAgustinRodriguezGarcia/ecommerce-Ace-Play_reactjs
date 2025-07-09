c4397d6c2682b47c377493d90ef485f69ac3a2d3
unknown <marg.ec256@gmail.com>
2025-07-09 02:37:20 -0300

    Comence a diseñar pagina de detalles de juego

--- src/components/juegos/JuegoData.jsx
+++ src/components/juegos/JuegoData.jsx
@@ -4,44 +4,85 @@ import { Juegos } from '../../pages/Juegos';
 
 export const JuegoData = () => {
   const { id } = useParams();
-  
+
   const [juego, setJuego] = useState(null);
+  const [juegoImages, setJuegoImages] = useState(null);
+  const [juegoVideos, setJuegoVideos] = useState(null);
   const [showLoading, setShowLoading] = useState(true);
 
-  const getJuego = async() => {
-    try{
+  const getJuego = async () => {
+    try {
       const response = await fetch(`https://api.rawg.io/api/games/${id}?key=957f6a2b15fa49f68a9bb400ac60e7f0`);
       const data = await response.json();
       setJuego(data);
+
+      const responseImg = await fetch(`https://api.rawg.io/api/games/${id}/screenshots?key=957f6a2b15fa49f68a9bb400ac60e7f0`)
+      const dataImages = await responseImg.json();
+      setJuegoImages(dataImages);
+
+      const responseVideos = await fetch(`https://api.rawg.io/api/games/${id}/movies?key=957f6a2b15fa49f68a9bb400ac60e7f0`)
+      const dataVideos = await responseVideos.json();
+      setJuegoVideos(dataVideos);
+
+      const tags = await fetch(`https://api.rawg.io/api/developers?key=957f6a2b15fa49f68a9bb400ac60e7f0`)
+      const dataTags = await tags.json()
+      
+      console.log('JUEGO CATOS COMPLETOS: ');
       console.log(data);
+      console.log('VIDEOS DE JUEGO: ');
+      console.log(dataVideos);
+      console.log('SCREENSHOTS DE JUEGO: ');
+      console.log(dataImages);
+      console.log(dataTags)
     }
-    catch (error){
-      console.error("Error fetching games:", error)
+    catch (error) {
+      console.error("Error fetching games:", error);
     }
     finally {
-      setShowLoading(false)
+      setShowLoading(false);
     }
   }
 
-  useEffect( () => {
-    window.scrollTo(0,0)
-    setShowLoading(false)
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setShowLoading(false);
     getJuego();
-  },[]);
+  }, []);
 
   return (
     <section className='data'>
-      {juego ? 
-      <section className='data_juego'>
-        <div className='data_juego_info'>
-          <h1>{juego.name}</h1>
-          <p>{juego.description_raw}</p>
+      {juego ?
+        <>
+          <div className='data_background'>
+            <img src={juego.background_image} alt="" />
+          </div>
+          <section className='data_juego'>
+            <div className='data_juego_info'>
+              <div className='data_juego_info_cont'>
+                <h1>{juego.name}</h1>
+                <img src={juego.background_image} alt="" />
+                <img src={juego.background_image} alt="" />
+              </div>
+              <div className='data_juego_info_cta'>
+                <ul className='data_juego_info_cta_list'>
+                  <li>Desarrollador <h5>{juego.developers[0].name}</h5></li>
+                  <li>Editor <h5>{juego.publishers[0].name}</h5></li>
+                  <li>Lanzamiento <h5>{juego.released}</h5></li>
+                  <li>Plataformas <h5>{juego.parent_platforms[0].platform.name}</h5></li>
+                </ul>
+                <div className='data_juego_info_cta_btns'>
+                  <button>Comprar ahora</button>
+                  <button>Agregar a Favoritos <i className="bi bi-heart"></i></button>
+                  <button>Agregar a Carrito <i className="bi bi-cart"></i></button>
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
+        :
+        <div className='juegos_loading'>
+          <h2 className='degrade' data-text='Cargando...'>Cargando...</h2>
         </div>
-      </section>
-      : 
-      <div className='juegos_loading'>
-        <h2 className='degrade' data-text='Cargando...'>Cargando...</h2>
-      </div>
       }
     </section>
   )

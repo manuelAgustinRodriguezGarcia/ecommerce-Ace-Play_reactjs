a3d8d54c0bd38bdb9ba6f15c5fa951c7ea6e9495
unknown <marg.ec256@gmail.com>
2025-07-16 16:34:36 -0300

    Agregue funcionalidad a agregar a favoritos y modifque diseño de pagina de juegos principal cambiando de 4 a 5 filas con 4 columnas y cambie pagina de favoritos

--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import ItemFav from "../components/ItemFav";
 import EmptyFav from "../components/EmptyFav";
 import { FavoritesContext } from '../contexts/FavoritesContext';
+import { EmptyPage } from '../components/EmptyPage'
 
 export const Favoritos = () => {
 
@@ -11,6 +12,7 @@ export const Favoritos = () => {
   return (
     <section className="favoritos">
       <div className="favoritos-list">
+        <EmptyPage></EmptyPage>
         {
           favList.length == 0 ? <EmptyFav></EmptyFav>
             :
